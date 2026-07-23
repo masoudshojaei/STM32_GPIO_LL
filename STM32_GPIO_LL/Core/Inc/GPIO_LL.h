5fab0d67bae48a5b50f1ed7075df426b965ea7c0
Masoud Shoajei <shojaei.mas@gmail.com>
2026-07-23 20:16:34 +0200

    Adding Button_init() , and Button_read() functions

--- STM32_GPIO_LL/Core/Inc/GPIO_LL.h
+++ STM32_GPIO_LL/Core/Inc/GPIO_LL.h
@@ -10,4 +10,7 @@
 
 void LED_init(void);
 void LED_Blink_Fast(void);
+void Button_init(void);
+uint8_t Button_read(void);
+void LED_OFF(void);
 #endif /* INC_GPIO_LL_H_ */
--- STM32_GPIO_LL/Core/Src/GPIO_LL.c
+++ STM32_GPIO_LL/Core/Src/GPIO_LL.c
@@ -31,3 +31,25 @@ void LED_Blink_Fast(void)
     //LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_5);     /* Turn on PA5 */
     for (volatile int i = 0; i < 50000; i++); /* Delay */
 }
+/* Adding Button initialization on board bush button PC13 */
+void Button_init(void)
+{
+    /* Enable GPIOC Clock */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+
+    /* PC13 Configuration */
+
+    // Pin mode: input
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_INPUT);
+
+    // Pull-up: button is active-low
+    LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_13, LL_GPIO_PULL_UP);
+}
+uint8_t Button_read(void)
+{
+    return LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_13) ? 0 : 1; /* Return 1 if button is pressed, 0 otherwise */
+}
+void LED_OFF(void)
+{
+    LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_5); /* Turn off PA5 */
+}
